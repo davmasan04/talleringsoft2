9bfc435b256578fbdcc1d5cd46fd101f7b211fc7
David Sanchez <davmasan@espol.edu.ec>
2023-06-08 23:21:39 -0500

    ejercicio realizado

--- src/main/java/labcodeinspection/Email.java
+++ src/main/java/labcodeinspection/Email.java
@@ -2,16 +2,48 @@ package labcodeinspection;
 
 public class Email {
 
-	private String m_firstName; 
-	private String m_lastName; 
-	private String password = null; 
-	private String department; 
-	private int defaultpasswordLength = 8; 
-	private String email; 
+	private String m_firstName;
+	private String m_lastName;
+	private String password = "";
+	private String department;
+	private int defaultpasswordLength = 8;
+	private String email;
+
+	public String getM_firstName() {
+		return m_firstName;
+	}
+
+	public void setM_firstName(String m_firstName) {
+		this.m_firstName = m_firstName;
+	}
+	
+	public void setM_lastName(String m_lastName) {
+		this.m_lastName = m_lastName;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public void setDefaultpasswordLength(int defaultpasswordLength) {
+		this.defaultpasswordLength = defaultpasswordLength;
+	}
+	
+	
+
+	public String getM_lastName() {
+		return m_lastName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
 
 	public Email(String firstName, String lastName) {
 		this.m_firstName = firstName;
-		this.m_lastName = lastName; //NOPMD
+		this.m_lastName = lastName; // NOPMD
 	}
 
 	public void showInfo() {
@@ -27,7 +59,7 @@ public class Email {
 		case 2:
 			this.department = "dev";
 			break;
-		case 3:
+		default:
 			this.department = "acct";
 			break;
 		}
@@ -40,7 +72,7 @@ public class Email {
 			int rand = (int) (Math.random() * set.length());
 			password[i] = set.charAt(rand);
 		}
-		return new String(password);
+		return password.toString();
 	}
 
 	public void generateEmail() {
